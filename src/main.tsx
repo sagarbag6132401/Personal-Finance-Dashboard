9be242015d468271b529b0c97c311d7d050492df
Sagar <sagarbag6132401@gmail.com>
2025-09-09 15:35:04 +0530

    Updated CSS

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import './main.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
